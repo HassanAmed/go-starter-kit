0fdc27a63da2cbee0c3bd1e024c8ed6c88cbc86a
HassanAmed <hassan4621@gmail.com>
2021-10-31 08:46:01 +0500

    Added category apis

--- routers/router.go
+++ routers/router.go
@@ -9,10 +9,12 @@ func InitRoutes(a *controllers.App) *controllers.App {
 	// a := controllers.App{}
 	a.Router = gin.Default()
 	router := a.Router
+	router.GET("/products", a.GetAllProducts)
 	router.GET("/product/:id", a.GetProduct)
 	router.POST("/product", a.CreateProduct)
 	router.PUT("/product/:id", a.UpdateProduct)
 	router.DELETE("/product/:id", a.DeleteProduct)
-
+	router.GET("/category/:id", a.GetCategory)
+	router.POST("/category", a.CreateCategory)
 	return a
 }
