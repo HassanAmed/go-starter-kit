9d0801979f9affd8a65db452342ca940dd1d5378
HassanAmed <hassan4621@gmail.com>
2021-11-01 17:05:30 +0500

    Add validations

--- models/credentials.go
+++ models/credentials.go
@@ -1,6 +1,6 @@
 package models
 
 type Credentials struct {
-	Username string `json:"username"`
-	Password string `json:"password"`
+	Username string `json:"username" binding:"required"`
+	Password string `json:"password" binding:"required,gte=0"`
 }
--- models/product.go
+++ models/product.go
@@ -6,8 +6,8 @@ import (
 
 type Product struct {
 	gorm.Model
-	Name       string   `gorm:"size:100;not null;unique"  json:"name"`
-	Price      float64  `gorm:"type:decimal(12,2)"        json:"price"`
-	CategoryID uint     `json:"categoryId"`
-	Category   Category `gorm:"foreignkey:CategoryID" binding:"required"     json:"-"`
+	Name       string   `gorm:"size:100;not null;unique"  json:"name" binding:"min=2,max=10"`
+	Price      float64  `gorm:"type:decimal(12,2)"       json:"price" binding:"gt=0"`
+	CategoryID uint     `json:"categoryId" binding:"required,gt=0"`
+	Category   Category `gorm:"foreignkey:CategoryID"    json:"-"`
 }
